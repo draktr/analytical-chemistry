094bc2259e3b514d97f50c90d742d8c153d33c59
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-28 16:13:50 +0100

    Fixed structure, formating and styling

--- plate-theory-plotting.R
+++ plate-theory-plotting.R
@@ -1,13 +1,28 @@
 # Plate Theory plotting
-t = 120
-mu = 0.5
-p = seq(0,100,length.out = 101)
 
-# the plate total
-M_pt = factorial(t)/(factorial(t-p)*factorial(p))*mu^p*(1-mu)^(t-p)
-plot(x=p, y=M_pt, ylab = 'M(p,t)', col='black')
+# Equilibriation stages
+t <- 120
+# Fraction of amount of substance in mobile phase
+mu <- 0.5
+# Plates
+p <- seq(0, 100, length.out = 101)
 
-# as p and t approach infinity, M_pt approaches normal distribution 
-#(continuous exponential curve)
-M_pt_cont = 1/sqrt(2*pi*mu*(1-mu)*t)*exp(-(mu*t-p)^2/(2*mu*(1-mu)*t))
-plot(x=p, y=M_pt_cont, type = 'l')
+# The plate total
+M_pt <- function(p, t, mu) {
+  M_pt <- factorial(t) / (factorial(t - p) * factorial(p)) * mu^p * (1 - mu)^(t - p)
+  return(M_pt)
+}
+
+# The plate total as $p, r \to \infty$
+# as p and t approach infinity, M_pt approaches normal distribution
+# (continuous exponential curve)
+M_pt_cont <- function(p, t, mu) {
+  M_pt_cont <- 1 / sqrt(2 * pi * mu * (1 - mu) * t) * exp(-(mu * t - p)^2 / (2 * mu * (1 - mu) * t))
+  return(M_pt_cont)
+}
+
+M <- M_pt(p, t, mu)
+plot(x = p, y = M, ylab = "M(p,t)", xlab = "p", col = "black")
+
+M_cont <- M_pt_cont(p, t, mu)
+plot(x = p, y = M_cont, type = "l")
